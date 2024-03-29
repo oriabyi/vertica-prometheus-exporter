e1c2e8ed9450707afdb30675fa3c2d1dd1417641
oriabyi <oleksandr.riabyi.dev@gmail.com>
2024-03-29 15:59:24 +0200

    Create query_request_labels.go

---
+++ monitoring/query_request_labels.go
@@ -0,0 +1,46 @@
+package monitoring
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/jmoiron/sqlx"
+)
+
+// QueryRequestLabel lists query performance metrics on the request_label level.
+type QueryRequestLabel struct {
+	RequestLabel      string `db:"request_label"`
+	RequestDurationMS int    `db:"request_duration_ms"`
+	MemoryAcquiredMB  int    `db:"memory_acquired_mb"`
+}
+
+// NewQueryRequestLabels returns query performance for all users.
+func NewQueryRequestLabels(db *sqlx.DB) []QueryRequestLabel {
+	sql := `
+	SELECT
+		request_label, 
+		SUM(COALESCE(request_duration_ms,0))::INT request_duration_ms, 
+		SUM(COALESCE(memory_acquired_mb,0))::INT memory_acquired_mb 
+	FROM v_monitor.query_requests 
+	GROUP BY request_label;
+	`
+
+	queryRequests := []QueryRequestLabel{}
+	err := db.Select(&queryRequests, sql)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return queryRequests
+}
+
+// ToMetric converts QueryRequestLabel to a Map.
+func (qr QueryRequestLabel) ToMetric() map[string]float32 {
+	metrics := map[string]float32{}
+
+	request_label := fmt.Sprintf("request_label=%q", qr.RequestLabel)
+	metrics[fmt.Sprintf("vertica_request_duration_ms{%s}", request_label)] = float32(qr.RequestDurationMS)
+	metrics[fmt.Sprintf("vertica_memory_acquired_mb{%s}", request_label)] = float32(qr.MemoryAcquiredMB)
+
+	return metrics
+}
